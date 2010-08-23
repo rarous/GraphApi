2f4dd1e88e4a7a538616e918323bfb9c372ba80e
rarous <roubicek@twareg.cz>
2010-08-23 18:15:38 +0200

    Přidány extenze pro hledání všech objektů grafu

--- src/Facebook.GraphApi/Objects/SearchExtensions.cs
+++ src/Facebook.GraphApi/Objects/SearchExtensions.cs
@@ -9,15 +9,45 @@ namespace Facebook.GraphApi.Objects {
       return graphApi.Search(args)["data"].Deserialize<T[]>();
     }
 
-    public static IEnumerable<Checkin> SearchCheckin(this Graph graphApi, int limit, int offset) {
-      return graphApi.SearchCheckin(limit, offset, null, null);
+    public static IEnumerable<Event> SearchEvents(this Graph graphApi, string query, int? limit, int? offset, DateTime? since, DateTime? until) {
+      return graphApi.Search<Event>(new SearchArguments {
+        Query = query,
+        Type = SearchType.Event,
+        Limit = limit,
+        Offset = offset,
+        Since = since,
+        Until = until,
+      });
     }
 
-    public static IEnumerable<Checkin> SearchCheckin(this Graph graphApi, int limit, int offset, DateTime since) {
-      return graphApi.SearchCheckin(limit, offset, since, null);
+    public static IEnumerable<Event> SearchEvents(this Graph graphApi, string query, int limit, int offset) {
+      return graphApi.SearchEvents(query, limit, offset, null, null);
     }
 
-    public static IEnumerable<Checkin> SearchCheckin(this Graph graphApi, int? limit, int? offset, DateTime? since, DateTime? until) {
+    public static IEnumerable<Event> SearchEvents(this Graph graphApi, string query, int limit, int offset, DateTime since) {
+      return graphApi.SearchEvents(query, limit, offset, since, null);
+    }
+
+    public static IEnumerable<Group> SearchGroups(this Graph graphApi, string query, int? limit, int? offset, DateTime? since, DateTime? until) {
+      return graphApi.Search<Group>(new SearchArguments {
+        Query = query,
+        Type = SearchType.Group,
+        Limit = limit,
+        Offset = offset,
+        Since = since,
+        Until = until,
+      });
+    }
+
+    public static IEnumerable<Group> SearchGroups(this Graph graphApi, string query, int limit, int offset) {
+      return graphApi.SearchGroups(query, limit, offset, null, null);
+    }
+
+    public static IEnumerable<Group> SearchGroups(this Graph graphApi, string query, int limit, int offset, DateTime since) {
+      return graphApi.SearchGroups(query, limit, offset, since, null);
+    }
+
+    public static IEnumerable<Checkin> SearchCheckins(this Graph graphApi, int? limit, int? offset, DateTime? since, DateTime? until) {
       return graphApi.Search<Checkin>(new SearchArguments {
         Type = SearchType.Checkin,
         Limit = limit,
@@ -27,19 +57,15 @@ namespace Facebook.GraphApi.Objects {
       });
     }
 
-    public static IEnumerable<Page> SearchPage(this Graph graphApi, string query) {
-      return graphApi.SearchPage(query, null, null, null, null);
+    public static IEnumerable<Checkin> SearchCheckins(this Graph graphApi, int limit, int offset) {
+      return graphApi.SearchCheckins(limit, offset, null, null);
     }
 
-    public static IEnumerable<Page> SearchPage(this Graph graphApi, string query, int limit, int offset) {
-      return graphApi.SearchPage(query, limit, offset, null, null);
+    public static IEnumerable<Checkin> SearchCheckins(this Graph graphApi, int limit, int offset, DateTime since) {
+      return graphApi.SearchCheckins(limit, offset, since, null);
     }
 
-    public static IEnumerable<Page> SearchPage(this Graph graphApi, string query, int limit, int offset, DateTime since) {
-      return graphApi.SearchPage(query, limit, offset, since, null);
-    }
-
-    public static IEnumerable<Page> SearchPage(this Graph graphApi, string query, int? limit, int? offset, DateTime? since, DateTime? until) {
+    public static IEnumerable<Page> SearchPages(this Graph graphApi, string query, int? limit, int? offset, DateTime? since, DateTime? until) {
       return graphApi.Search<Page>(new SearchArguments {
         Query = query,
         Type = SearchType.Page,
@@ -48,6 +74,52 @@ namespace Facebook.GraphApi.Objects {
         Since = since,
         Until = until,
       });
+    }
+
+    public static IEnumerable<Page> SearchPages(this Graph graphApi, string query, int limit, int offset) {
+      return graphApi.SearchPages(query, limit, offset, null, null);
+    }
+
+    public static IEnumerable<Page> SearchPages(this Graph graphApi, string query, int limit, int offset, DateTime since) {
+      return graphApi.SearchPages(query, limit, offset, since, null);
+    }
+
+    public static IEnumerable<Post> SearchPosts(this Graph graphApi, string query, int? limit, int? offset, DateTime? since, DateTime? until) {
+      return graphApi.Search<Post>(new SearchArguments {
+        Query = query,
+        Type = SearchType.Post,
+        Limit = limit,
+        Offset = offset,
+        Since = since,
+        Until = until,
+      });
+    }
+
+    public static IEnumerable<Post> SearchPosts(this Graph graphApi, string query, int limit, int offset) {
+      return graphApi.SearchPosts(query, limit, offset, null, null);
+    }
+
+    public static IEnumerable<Post> SearchPosts(this Graph graphApi, string query, int limit, int offset, DateTime since) {
+      return graphApi.SearchPosts(query, limit, offset, since, null);
+    }
+
+    public static IEnumerable<User> SearchUsers(this Graph graphApi, string query, int? limit, int? offset, DateTime? since, DateTime? until) {
+      return graphApi.Search<User>(new SearchArguments {
+        Query = query,
+        Type = SearchType.User,
+        Limit = limit,
+        Offset = offset,
+        Since = since,
+        Until = until,
+      });
+    }
+
+    public static IEnumerable<User> SearchUsers(this Graph graphApi, string query, int limit, int offset) {
+      return graphApi.SearchUsers(query, limit, offset, null, null);
+    }
+
+    public static IEnumerable<User> SearchUsers(this Graph graphApi, string query, int limit, int offset, DateTime since) {
+      return graphApi.SearchUsers(query, limit, offset, since, null);
     }
   }
 }
